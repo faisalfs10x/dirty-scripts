88b16f6b1718ccfa8ed51906b50266696b40504b
Faisalfs10x <51811615+faisalfs10x@users.noreply.github.com>
2020-12-29 18:43:25 +0800

    Update liveHostPortScan.sh

--- liveHostPortScan.sh
+++ liveHostPortScan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 # faisal May 2,2020
+# https://github.com/faisalfs10x
 # usage: 1. change the desire subnet value at line 13
 # 		 2. change the desire ports to be scanned in the array at line 27
 # requirement nc [v1.10-41.1+b1]
