504478cc424324d10cb1dd258686d4a8c0db8355
Faisalfs10x <51811615+faisalfs10x@users.noreply.github.com>
2021-01-07 04:08:57 +0800

    Create dnsAmp.sh
    DNS Amplification Attack test via openresolver

---
+++ DNS-Amplification-attack/dnsAmp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# https://github.com/faisalfs10x
+
+echo
+echo "DNS Amplification Attack test via openresolver"
+echo "Grep list of DNS IP from target ASN thru shodan-cli and test via openresolver"
+echo "Require Shodan-CLI : pip install -U --user shodan" #https://help.shodan.io/command-line-interface/0-installation
+echo "Example Usage: ./dnsAmp.sh AS15169" #AS15169 is Google ASN
+echo
+
+target_ASN=$1
+
+DNS_IP=$(shodan search asn:$1 port:53 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+for ip in $DNS_IP;
+do
+    TEST=$(dig +short +tries=1 +time=2 test.openresolver.com TXT @$ip | grep open-resolver-detected)
+    if [ -z "$TEST" ]; then
+
+        echo "$ip is good"
+    else
+    	echo -e "\e[93m$ip is vulnerable\e[0m";
+    	#echo -e "\e[0m"
+    fi
+done
